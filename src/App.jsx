1e3c85958c7f0a9debabf6d568566e3172356df1
Caleb Owatah <owatahcaleb@gmail.com>
2024-06-01 23:56:26 +0100

    feat: count down logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,44 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hour, setHour] = useState(0);
+  const [timer, setTimer] = useState("00:00:00");
+  const [timeInterval, setTimeInterval] = useState(null);
+
+  const handleTimer = (countDownTime) => {
+    let now = new Date().getTime();
+    let interval = countDownTime - now;
+
+    if (interval > 0) {
+      setHour(Math.floor(interval / (1000 * 60 * 60)));
+      setMinutes(Math.floor((interval % (1000 * 60 * 60)) / (1000 * 60)));
+      setSeconds(Math.floor((interval % (1000 * 60)) / 1000));
+    } else {
+      clearInterval(timeInterval);
+    }
+  };
+
+  const handleStart = () => {
+    const countDownTime = Date.now() + timer * 1000;
+    setTimeInterval(setInterval(handleTimer, 1000, countDownTime));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <input
+        type="text"
+        placeholder="input time"
+        onChange={(e) => setTimer(e.target.value)}
+      />
+      <button onClick={handleStart}>start</button>
+
+      <h1>
+        {hour}:{minutes}:{seconds}
+      </h1>
+    </div>
   );
 }
 
